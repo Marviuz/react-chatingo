c82866404549952c7baf55abd9d0f8eed0db34e3
Marcus <2161524@slu.edu.ph>
2018-08-20 16:41:33 +0800

    MODIFIED: chat bubble styles and ADDED: placeholder for iframe

--- components/ChatMe.jsx
+++ components/ChatMe.jsx
@@ -2,6 +2,7 @@ const ChatMe = (props) => (
   <div className="chat-message me">
     <div className="chat-bubble">
       <div className="chat-user">{props.name}</div>
+      {/* <iframe className="chat-yt" src="https://www.youtube.com/embed/J5ntXqQtK1E" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe> */}
       <p className="chat-content">{props.message}</p>
       <div className="chat-time">{props.timeDate}</div>
     </div>
--- components/ChatOthers.jsx
+++ components/ChatOthers.jsx
@@ -5,6 +5,7 @@ const ChatOthers = (props) => (
     </div>
     <div className="chat-bubble">
       <div className="chat-user">{props.name}</div>
+      {/* <iframe className="chat-yt" src="https://www.youtube.com/embed/J5ntXqQtK1E" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe> */}
       <p className="chat-content">{props.message}</p>
       <div className="chat-time">{props.timeDate}</div>
     </div>
